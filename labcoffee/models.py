ac7fc00453b090e1a9cb2b9c1a9d94c878dab5bc
Taciano Morais Silva <tacianosilva@gmail.com>
2020-06-09 23:19:02 -0300

    Criando os modelos do app labcoffee.

--- labcoffee/models.py
+++ labcoffee/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.first_name} {self.last_name}'
+
+
+class Presentation(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True)
+    description = models.TextField(max_length=500, help_text='Enter a brief description of the presentation')
+    start_date = models.DateTimeField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.title} - {self.author}'
+
+
