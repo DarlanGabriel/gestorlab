f06bc5e6e6ae4c03a3813bd6a804d71c923150ff
Taciano Morais Silva <tacianosilva@gmail.com>
2020-07-16 19:32:41 -0300

    Add atributos no model Author e Presentation.

--- labcoffee/models.py
+++ labcoffee/models.py
@@ -6,10 +6,11 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
+    organizacao = models.CharField(max_length=100)
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.first_name} {self.last_name} - {self.organizacao}'
 
 
 class Presentation(models.Model):
@@ -17,9 +18,10 @@ class Presentation(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, blank=True)
     description = models.TextField(max_length=500, help_text='Enter a brief description of the presentation')
     start_date = models.DateTimeField()
+    status = models.CharField(max_length=15)
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.title} - {self.author}'
+        return f'{self.title} ({self.author})'
 
 
