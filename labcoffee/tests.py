c349f5ac32e15d153b8693bd9c208ca5780996df
Taciano Morais Silva <tacianosilva@gmail.com>
2020-07-17 13:42:17 -0300

    Add test do model Author.

--- labcoffee/tests.py
+++ labcoffee/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from labcoffee.models import Author
+
+
+class AuthorTests(TestCase):
+
+    def test_new_author(self):
+        autor = Author(first_name='Zé', last_name='Silva',
+                       email='ze@silva.com', organizacao='Zé Corp')
+
+        self.assertEqual(autor.first_name, 'Zé', 'Testa Nome')
+        self.assertEqual(autor.last_name, 'Silva', 'Testa Sobrenome')
+        self.assertEqual(str(autor), 'Zé Silva - Zé Corp', 'Testa Nome')
