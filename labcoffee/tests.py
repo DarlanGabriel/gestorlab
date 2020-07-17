191bbb042bc200d79d57581ac634c23e6ef8c9c1
Taciano Morais Silva <tacianosilva@gmail.com>
2020-07-17 14:39:03 -0300

    Add test do model Presentation.

--- labcoffee/tests.py
+++ labcoffee/tests.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from django.test import TestCase
+import django
+django.setup()
+from labcoffee.models import Author, Presentation
 
-from labcoffee.models import Author
 
-
-class AuthorTests(TestCase):
+class ModelTests(TestCase):
 
     def test_new_author(self):
         autor = Author(first_name='Zé', last_name='Silva',
@@ -12,3 +15,12 @@ class AuthorTests(TestCase):
         self.assertEqual(autor.first_name, 'Zé', 'Testa Nome')
         self.assertEqual(autor.last_name, 'Silva', 'Testa Sobrenome')
         self.assertEqual(str(autor), 'Zé Silva - Zé Corp', 'Testa Nome')
+
+    def test_new_presentation(self):
+        apresentacao = Presentation(title='Usando Testes de Unidade', description='Testes de Unidade no Python',
+                                    start_date=datetime.now, status='Planejado')
+        autor = Author(first_name='Zé', last_name='Silva',
+                       email='ze@silva.com', organizacao='Zé Corp')
+        apresentacao.author = autor
+
+        self.assertEqual(str(apresentacao), 'Usando Testes de Unidade (Zé Silva - Zé Corp)')
