8a6a545be5b7186ad2334a391e6ddab8be3588ab
Taciano Morais Silva <tacianosilva@gmail.com>
2020-07-26 11:08:53 -0300

    Atualização dos testes de unidade.

--- laboratorios/tests/test_urls.py
+++ laboratorios/tests/test_urls.py
@@ -1,21 +1,22 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from laboratorios.views import laboratorio_list, laboratorio_detail, LaboratorioCreateView
+from laboratorios.views import laboratorio_list, laboratorio_detail, LaboratorioCreateView, home
 
 
 class TestUrls(SimpleTestCase):
 
+    def test_home_url_is_resolved(self):
+        url = reverse('laboratorio_home')
+        self.assertEqual(resolve(url).func, home)
+
     def test_list_url_is_resolved(self):
         url = reverse('laboratorio_list')
-        print(resolve(url))
         self.assertEqual(resolve(url).func, laboratorio_list)
 
     def test_add_url_is_resolved(self):
         url = reverse('laboratorio_add')
-        print(resolve(url))
         self.assertEqual(resolve(url).func.view_class, LaboratorioCreateView)
 
     def test_detail_url_is_resolved(self):
         url = reverse('laboratorio_detail', args=['some-slug'])
-        print(resolve(url))
         self.assertEqual(resolve(url).func, laboratorio_detail)
--- laboratorios/tests/test_views.py
+++ laboratorios/tests/test_views.py
@@ -7,6 +7,7 @@ class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.home_url = reverse('laboratorio_home')
         self.list_url = reverse('laboratorio_list')
         self.detail_url = reverse('laboratorio_detail', args=['labtest'])
         Laboratorio.objects.create(
@@ -14,6 +15,12 @@ class TestViews(TestCase):
             sigla='labtest',
             descricao='fsfs sfsdv egwgw vfss'
         )
+
+    def test_laboratorio_home_GET(self):
+        response = self.client.get(self.home_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'laboratorios/laboratorio-list.html')
 
     def test_laboratorio_list_GET(self):
         response = self.client.get(self.list_url)
