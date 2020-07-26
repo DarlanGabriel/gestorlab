aab92e58573455a649761822e7077d4742df74b7
Taciano Morais Silva <tacianosilva@gmail.com>
2020-07-26 11:08:40 -0300

    Atualização das views do app laboratorios.

--- laboratorios/urls.py
+++ laboratorios/urls.py
@@ -3,8 +3,7 @@ from laboratorios import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-
+    path('', views.home, name='laboratorio_home'),
     path('list/', views.laboratorio_list, name='laboratorio_list'),
     path('add/', views.LaboratorioCreateView.as_view(), name='laboratorio_add'),
     path('<slug:laboratorio_slug>/', views.laboratorio_detail, name='laboratorio_detail')
--- laboratorios/views.py
+++ laboratorios/views.py
@@ -8,9 +8,6 @@ from laboratorios.models import Laboratorio, LinhaPesquisa, Departamento
 def home(request):
     """View function for home page of site."""
 
-    context = {
-    }
-
     return laboratorio_list(request)
 
 
@@ -22,15 +19,12 @@ def laboratorio_list(request):
 def laboratorio_detail(request, laboratorio_slug):
     laboratorio = get_object_or_404(Laboratorio, slug=laboratorio_slug)
 
-    if request.method == 'GET':
-        linha_pesquisa_list = LinhaPesquisa.objects.filter(laboratorio=laboratorio)
-        context = {
-            'laboratorio': laboratorio,
-            'linha_pesquisa_list': linha_pesquisa_list,
-        }
-        return render(request, 'laboratorios/laboratorio-detail.html', context=context)
-
-    return redirect(laboratorio)
+    linha_pesquisa_list = LinhaPesquisa.objects.filter(laboratorio=laboratorio)
+    context = {
+        'laboratorio': laboratorio,
+        'linha_pesquisa_list': linha_pesquisa_list,
+    }
+    return render(request, 'laboratorios/laboratorio-detail.html', context=context)
 
 
 class LaboratorioCreateView(CreateView):
